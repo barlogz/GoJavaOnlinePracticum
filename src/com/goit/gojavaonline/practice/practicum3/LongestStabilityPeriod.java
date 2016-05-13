85ab2bb9daf703bac7db18f2a118bb098d54bd6a
Roman <r.didkivskyi@gmail.com>
2016-05-13 14:31:39 +0300

    added Task 3.2 longest stability period working materials

--- src/com/goit/gojavaonline/practice/practicum3/LongestStabilityPeriod.java
+++ src/com/goit/gojavaonline/practice/practicum3/LongestStabilityPeriod.java
@@ -10,30 +10,21 @@ public class LongestStabilityPeriod {
             counter = 1;
         } else if (gdp.length == 3) {
             counter = 2;
-        } else {
-
-
-
-
-
+        } else if(gdp[gdp.length-1 ] >= gdp [0] && gdp[gdp.length-1] >= gdp [gdp.length-2]) {
             for (int i = 0; i < gdpLength; i++) {
-                if (gdp[i] <= gdp[0] + 1) {
+                if ((gdp[i] <= gdp[0] + 1) ) {
                     counter += 1;
-                } else {
-                    counter = 0;
-                    for (int j = 0; j < gdpLength; j++) {
-                        if (((gdp[j] + gdp[j + 1] + 2) == (gdp[j + 2] + gdp[j + 3])) && (gdp[j] == gdp[j + 1]) && (gdp[j + 2] == gdp[j + 3])) {
-                            counter += 2;
-                        } else if (((gdp[j] + gdp[j + 1] - 2) == (gdp[j + 2] + gdp[j + 3])) && (gdp[j] == gdp[j + 1]) && (gdp[j + 2] == gdp[j + 3])) {
-                            counter += 2;
-                        }
-                    }
-//                    return counter;
-
                 }
-//            }return counter;
             }
-//
+        } else {
+            counter = 0;
+            for (int j = 0; j < gdpLength; j++) {
+                if (((gdp[j] + gdp[j + 1] + 2) == (gdp[j + 2] + gdp[j + 3])) && (gdp[j] == gdp[j + 1]) && (gdp[j + 2] == gdp[j + 3])) {
+                    counter += 2;
+                } else if (((gdp[j] + gdp[j + 1] - 2) == (gdp[j + 2] + gdp[j + 3])) && (gdp[j] == gdp[j + 1]) && (gdp[j + 2] == gdp[j + 3])) {
+                    counter += 2;
+                }
+            }
         }
         return counter;
     }
