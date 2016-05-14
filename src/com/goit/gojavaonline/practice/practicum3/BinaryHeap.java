6f74e8ab9c9fd7abe831f0e8ef8512a4a32eb92b
Roman <r.didkivskyi@gmail.com>
2016-05-14 12:47:26 +0300

    added Task 3.5 BinaryHeap_WM

--- src/com/goit/gojavaonline/practice/practicum3/BinaryHeap.java
+++ src/com/goit/gojavaonline/practice/practicum3/BinaryHeap.java
@@ -1,16 +1,43 @@
 package com.goit.gojavaonline.practice.practicum3;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
 public class BinaryHeap {
+    private static final int d = 2;
+    private int heapSize;
+    private int[] heap;
 
     public BinaryHeap(int size) {
+        heapSize = 0;
+        heap = new int[size + 1];
+        Arrays.fill(heap, -1);
 
     }
 
     public void insert(int val) {
+        if (heapSize == heap.length)
+            throw new NoSuchElementException("Overflow Exception");
+        heap[heapSize++] = val;
+        heapifyUp(heapSize - 1);
+    }
 
+    private void heapifyUp(int childInd) {
+        int temp = heap[childInd];
+        while (childInd > 0 && temp < heap[parent(childInd)]){
+            heap[childInd] = heap[parent(childInd)];
+            childInd = parent(childInd);
+        }
+        heap[childInd] = temp;
+    }
+
+    private int parent(int childInd) {
+        return (childInd - 1) /d;
     }
 
     public int poll() {
-
+        Queue queue = new Queue;
+        queue.poll()
     }
 }
